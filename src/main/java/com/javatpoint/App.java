52d1428e8eb6cab95460ea2dfba950bf29f1b56e
sailaja <sailajavijayk@gmail.com>
2021-07-09 12:57:15 +0530

    changed

--- src/main/java/com/javatpoint/App.java
+++ src/main/java/com/javatpoint/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome Folks! ALOHA" );
+        System.out.println( "Welcome Folks AHOY ! ALOHA" );
     }
 }
