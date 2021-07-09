c0999a72ddbd31635e5faa4e11120c01dc57e4cb
sailaja <sailajavijayk@gmail.com>
2021-07-09 09:35:07 +0530

    Changed for training

--- src/main/java/com/javatpoint/App.java
+++ src/main/java/com/javatpoint/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to my world" );
+        System.out.println( "Welcome Folks! ALOHA" );
     }
 }
